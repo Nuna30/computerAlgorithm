620521b0e5f8250daad53fd46dda1ee50f44477a
Minhyeon <guncraft11@gmail.com>
2025-04-05 21:53:33 +0900

    .

--- 08 Space Station/main.cpp
+++ 08 Space Station/main.cpp
@@ -7,7 +7,8 @@
 
 using namespace std;
 
-const float float_epsilon = numeric_limits<float>::epsilon();
+const double double_epsilon = numeric_limits<double>::epsilon();
+const double DOUBLE_MAX = numeric_limits<double>::max();
 
 
 // ###### coords ###############
@@ -30,13 +31,13 @@ vector<vector<int>> coords;
 vector<vector<pair<int, int>>> params;
 
 // y = a + bt 계산
-float l(int a, int b, float t) {
+double l(int a, int b, double t) {
     return a + b * t;
 }
 
 // 매개변수 t, s로 xyz 좌표 계산
-tuple<float, float, float> calc_xyz(int station, float t) {
-    float x, y, z;
+tuple<double, double, double> calc_xyz(int station, double t) {
+    double x, y, z;
     x = l(params[station][0].first, params[station][0].second, t);   
     y = l(params[station][1].first, params[station][1].second, t);
     z = l(params[station][2].first, params[station][2].second, t);
@@ -44,45 +45,42 @@ tuple<float, float, float> calc_xyz(int station, float t) {
 }
 
 // 두 점 p1, p2 거리
-float distance(float x1, float y1, float z1, float x2, float y2, float z2) {
+double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
     return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
 }
 
 // 점 p에서 매개변수가 t인 곳과의 거리 계산
-float calc_distance(int x, int y, int z, int station, float t) {
+double calc_distance(double x, double y, double z, int station, double t) {
     auto [x2, y2, z2] = calc_xyz(station, t);
     return distance(x, y, z, x2, y2, z2);
 }
 
 // p1의 최단지점인 p2의 s 찾기
 // t_E1과 t_E2는 정거장의 양 끝점에 해당하는 매개변수
-float find_nextS(int x, int y, int z, int theOtherStation) {
+double find_nextS(double x, double y, double z, int theOtherStation) {
     auto [a, b] = params[theOtherStation][0];
-    float x2 = coords[theOtherStation][4];
+    double x2 = coords[theOtherStation][3];
 
-    float s_E1 = 0;
-    float s_E2 = (x2 - a) / b;
+    double s_E1 = 0;
+    double s_E2 = (x2 - a) / b;
 
     if (s_E2 < 0) {
-        float temp = s_E1;
+        double temp = s_E1;
         s_E1 = s_E2;
         s_E2 = temp;
     }
 
-    float s_left = s_E1;
-    float s = (s_E1 + s_E2) / 2;
-    float s_right = s_E2;
+    double s_left = s_E1;
+    double s = (s_E1 + s_E2) / 2;
+    double s_right = s_E2;
     
     while (true) { 
-        if (s - float_epsilon <= s_E1) return s_E1;
-        else if (s + float_epsilon >= s_E2) return s_E2;
+        if (abs(s - s_E1) <= double_epsilon) return s_E1;
+        else if (abs(s - s_E2) <= double_epsilon) return s_E2;
 
-        // 엡실론만큼만 검사하면 값이 안 변해서 10배 해줌
-        float sl_distance = calc_distance(x, y, z, theOtherStation, s - 10 * float_epsilon);
-        float s_distance = calc_distance(x, y, z, theOtherStation, s);
-        float sr_distance = calc_distance(x, y, z, theOtherStation, s + 10 * float_epsilon);
-        
-        printf("sl, s, sr : %.20lf, %.20lf, %.20lf\n", sl_distance, s_distance, sr_distance);
+        double sl_distance = calc_distance(x, y, z, theOtherStation, s - double_epsilon);
+        double s_distance = calc_distance(x, y, z, theOtherStation, s);
+        double sr_distance = calc_distance(x, y, z, theOtherStation, s + double_epsilon);
 
         if (sl_distance < s_distance) s_right = s;
         else if (sr_distance < s_distance) s_left = s;
@@ -94,21 +92,32 @@ float find_nextS(int x, int y, int z, int theOtherStation) {
 
 int find_minDistance() {
     int targetStation = 0;
-    float parameter = 0;
-    float prev_parameter = 0;
+    double t = 0;
+    double s = 0;
+    double prev_t = DOUBLE_MAX;
+    double prev_s = DOUBLE_MAX;
+    double parameter = t;
 
     while (true) {
         auto [x, y, z] = calc_xyz(targetStation, parameter);
-        targetStation = (targetStation + 1) % 2;
-        prev_parameter = parameter;
+
+        targetStation = ((targetStation == 0) ? 1 : 0);
+
         parameter = find_nextS(x, y, z, targetStation);
-        printf("p : (%lf, %lf) \n", parameter, prev_parameter);
-        if (abs(parameter - prev_parameter) <= 0.1) {
-            auto [x1, y1, z1] = calc_xyz(targetStation, parameter);
-            targetStation = (targetStation + 1) % 2;
-            auto [x2, y2, z2] = calc_xyz(targetStation, prev_parameter);
-            printf("(%lf, %lf, %lf), (%lf, %lf, %lf)\n", x1, y1, z1, x2, y2, z2);
-            float minDistance = distance(x1, y1, z1, x2, y2, z2);
+
+        if (targetStation == 0) {
+            prev_t = t;
+            t = parameter;
+        }
+        else {
+            prev_s = s;
+            s = parameter;
+        }
+
+        if (abs(prev_t - t) <= double_epsilon && abs(prev_s - s) <= double_epsilon) {
+            auto [x1, y1, z1] = calc_xyz(0, t);
+            auto [x2, y2, z2] = calc_xyz(1, s);
+            double minDistance = distance(x1, y1, z1, x2, y2, z2);
             return ceil(minDistance);
         }
     }
@@ -132,12 +141,9 @@ int main() {
 
     cout << find_minDistance() << '\n';
 
-    케이스 1, 5는 맞고 2, 3, 4는 틀림.
-    어째서?
-    좌표 위에 직접 점 찍어서 확인해볼 것
-    -8 -9 -8
-    -10 0 3 이 두 점이 최단거리로 보임 근데 계산하면 14.~인데 
-    답은 9임 이 두 점이 왜 최단거리가 아닌지 알아내야 함
-
     return 0;
 }
+
+토큰 800개를 300개로 줄이기???
+
+그냥 수학식으로 풀자....
