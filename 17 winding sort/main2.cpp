43e773b714485010f6c577c62eb787f4f4d2b477
s202255656 <s202255656@pusan.ac.kr>
2025-06-19 23:14:46 +0900

    sorting 점 후보 선정 논리 수정
    - ang > pa에서 = 등호를 넣지 않았던 것이 문제
    - 등호를 넣지 않으면 이전 점과 같은 일직선 상에 있었을 경우 다음
      시행에서 선택되지 않음

--- 17 winding sort/main2.cpp
+++ 17 winding sort/main2.cpp
@@ -34,7 +34,13 @@ bool signed_area(const point& p, const point& q, set<point>& S) {
         if (p.idx == s.idx || q.idx == s.idx) continue;
         int cross = crossProduct(p, q, s);
         if (cross > 0) positiveSign++;
-        else                  negativeSign++;
+        else if (cross < 0) {
+            negativeSign++;
+            // if (q.idx == 2) printf("\npi, qi, si : %d, %d, %d\n", p.idx, q.idx, s.idx);
+        }
+    }
+    if (q.idx == 2) {
+        // cout << "sign : " << positiveSign << " " << negativeSign << endl;
     }
     if (positiveSign > 0 && negativeSign > 0) return false;
     else                                      return true;
@@ -61,8 +67,10 @@ int main() {
                 double ang = rad * 180 / PI;
                 if (ang < 0) ang += 360;
                 candidates.push_back({ang, q});
+                printf("idx, ang : %d, %.2lf\n", q.idx, ang);
             }
         }
+        cout << '\n';
         if (candidates.size() == 0) return 1;
         auto& [min_ang, min_point] = candidates[0];
         if (candidates.size() > 1) {
@@ -71,12 +79,14 @@ int main() {
                 const auto& [ang, q] = candidates[i];
                 if (crossProduct(p, min_point, q) == 0) {
                     if (calcDistance(p, q) < calcDistance(p, min_point)) {
+                        printf("#1 p : %d, %d -> %d\n", p.idx, min_point.idx, q.idx);
                         min_ang = ang;
                         min_point = q;
                         lab = true;
                     }
                 }
-                else if (ang > pa && ang < min_ang) {
+                else if (ang >= pa && ang < min_ang) {
+                    printf("#2 p: %d, %d -> %d\n", p.idx, min_point.idx, q.idx);
                     min_ang = ang;
                     min_point = q;
                     lab = true;
